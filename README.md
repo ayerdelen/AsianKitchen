77aebd0012d9684ee40f9b4e22307414a6db7d63
aycan <aycanyerdelen@gmail.com>
2021-02-03 14:21:40 +0200

    Asian-Kitchen done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Asian-Kitchen
+
+[Click to see](https://ayerdelen.github.io/Asian-Kitchen/)
+
+- This is a personal project. I made restaurant menu and filter it by country.
+
+With this project I've learned:
+
+- Filter, map and reduce methods,
+- Creating and returning data,
+- Creating "All" buttons and return function with it.
+
+![.](ss.png)
--- app.js
+++ app.js
@@ -81,11 +81,52 @@ const menu = [
     desc: `Red bean paste dessert, serving with honey.`,
   },
 ];
+
 const section = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+
+const categories = menu.reduce(
+  (values, item) => {
+    if (!values.includes(item.category)) {
+      values.push(item.category);
+    }
+    return values;
+  },
+  ["All"]
+);
+
+const categoryList = () => {
+  const categoryBtns = categories
+    .map((category) => {
+      return `<button class="btn btn-outline-dark btn-item" data-id=${category}>${category}</button>`;
+    })
+    .join("");
+
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll(".btn-item");
+
+  //filter menu
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const category = e.currentTarget.dataset.id;
+      console.log(category);
+      const menuCategory = menu.filter((menuItem) => {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "All") {
+        menuList(menu);
+      } else {
+        menuList(menuCategory);
+      }
+    });
+  });
+};
 
 const menuList = (menuItems) => {
   let displayMenu = menuItems.map((item) => {
-    return `<div class="menu-items col-6">
+    return `<div class="menu-items col-lg-6 col-sm-12">
             <img
               src=${item.img}
               alt=${item.title}
@@ -108,3 +149,4 @@ const menuList = (menuItems) => {
 };
 
 menuList(menu);
+categoryList();
